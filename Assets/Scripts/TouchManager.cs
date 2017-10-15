ce0451eef2fd8722328518b04e911383446f9274
unknown <aayushmallik@outlook.com>
2017-10-15 21:31:16 +0545

    Commiting the file

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+	private float InitialTouchTime;
+	private float FinalTouchTime;
+	private Vector2 InitialTouchPosition;
+	private Vector2 FinalTouchPosition;
+
+	private float XaxisForce; //Checking velocity in X-axis
+	private float YaxisForce; //Checking velocity in Y axis
+	private float ZaxisForce; //Checking velocity in Z axis
+
+	private Vector3 Requireforce;
+
+	public Rigidbody ball;
+
+	public bool canswipe = true;
+	void Start()
+	{
+		Time.timeScale = 3; //to increase gameplay by 3 times
+		ball.useGravity = false; //unless user acts on the ball gravity should be neglected
+	}
+
+	public void OnTouchDown() //For the mouse being pressed
+	{
+		if (canswipe) {
+			InitialTouchTime = Time.time;
+			InitialTouchPosition = Input.mousePosition;
+		}
+	}
+
+	public void OnTouchUp() //After lifting the mouse
+	{
+		if (canswipe) {
+			FinalTouchTime = Time.time;
+			FinalTouchPosition = Input.mousePosition;
+			Ballthrow ();
+		}
+	}
+
+	private void Ballthrow()
+	{
+		XaxisForce = FinalTouchPosition.x - InitialTouchPosition.x;//force on x
+		YaxisForce = FinalTouchPosition.y - InitialTouchPosition.y;
+		ZaxisForce = FinalTouchTime - InitialTouchTime;
+
+		Requireforce = new Vector3 (-XaxisForce, YaxisForce/2.0f, -ZaxisForce*90f); //we divide to make sure that slight swipe is enough to make it work
+		ball.useGravity = true;
+		ball.isKinematic = false;
+		ball.velocity = Requireforce;
+		canswipe = false;
+	}
+		
+}
---
+++ Assets/Scripts/basketball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class basketball : MonoBehaviour {
+
+	public Text score;
+	public Text remainingballs;
+
+	private int currentscore = 0;
+	public int remainingballs_number = 50; 
+	private Vector3 InitialPosition;
+	private TouchManager touchsystem;
+
+	void Start()
+	{
+		touchsystem = GameObject.FindObjectOfType<TouchManager>().GetComponent<TouchManager>();
+		InitialPosition = this.transform.position; //helps to set initial position to current ball position
+		ChangeText();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Ring") 
+		{
+			ScoreUpdate();
+			remainingballs_number++;
+		}
+	}
+
+	public void DecreaseRemainingBalls()
+	{
+		remainingballs_number--;
+		ChangeText ();
+	}
+
+	public void ChangeText()
+	{
+		remainingballs.text = "Balls left: " + remainingballs_number.ToString ();
+	}
+	private void ScoreUpdate()
+	{
+		currentscore++;
+		score.text = currentscore.ToString();
+	}
+
+	public void ResetPosition()
+	{
+		this.transform.position = InitialPosition + new Vector3 (Random.Range (-50f, 50f), 0f, 0f);
+		this.GetComponent<Rigidbody> ().useGravity = false;
+		this.GetComponent<Rigidbody> ().isKinematic = true;
+		this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		touchsystem.canswipe = true;
+	}
+}
+
